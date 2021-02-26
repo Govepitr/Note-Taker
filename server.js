4a7b3efc26d43523dbe0071ae2ac1e5521310351
Dan Arbelo <govepitr@gmail.com>
2021-02-25 19:30:26 -0700

    add a bit more to the server.js

--- server.js
+++ server.js
@@ -3,4 +3,20 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = 
+const port = 4040;
+const mainDir = path.join(__dirname, "/public");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+
+
+
+app.listen(port, function() {
+  console.log(`Welcome, to The Grid..I mean Port ${port}. Tron, gotta love it.`);
+})
