6675299245facba5ed19856828a987cbf7cbe37b
Dan Arbelo <govepitr@gmail.com>
2021-02-27 19:30:19 -0700

    some minor icon/test tweaking

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Note Taker Starter Code
+
+
+Click on the Galactic Empire icon to begin a new orde...err..note. New Note
+Jedi Council icon will save your note.
+
+To run server:
+nodemon server
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -152,7 +152,7 @@ const renderNoteList = async (notes) => {
   }
 
   if (jsonNotes.length === 0) {
-    noteListItems.push(createLi('No saved Notes', false))
+    noteListItems.push(createLi('Your notes are in another castle!', false))
   }
 
   jsonNotes.forEach((note) => {
